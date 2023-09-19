996ab17d7d703ec0e3b91a00650fa6169c85baa0
Felix Lisczyk <felix.lisczyk@gmail.com>
2023-09-19 12:06:58 +0200

    Updated Realm to 10.42.3

--- Package.swift
+++ Package.swift
@@ -3,11 +3,11 @@
 import PackageDescription
 
 func buildTargets() -> [Target] {
-    let baseURL = "https://github.com/realm/realm-swift/releases/download/v10.42.2"
+    let baseURL = "https://github.com/realm/realm-swift/releases/download/v10.42.3"
     let xcodeVersion = "15.0"
     
-    let realmChecksum = "b3eee3236de1991cfe6ab3072052c33e4030c01154216bfab94224b2ca92b848"
-    let realmSwiftChecksum = "228b7d129e946cb1a839bd31ac07d4b3713dd89bca01b92b6abccb4756b16a80"
+    let realmChecksum = "814d0d58baf6d890fe916db317d58f6be634a0e340ff06f0e4d834e327cbad74"
+    let realmSwiftChecksum = "dceee0c30d6111715f5d97313aedcbb5fb072aca50fefecf1341d826b8f06002"
     
     return [
         .binaryTarget(
