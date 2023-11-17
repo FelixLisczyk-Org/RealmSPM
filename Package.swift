4e15af15f65ef24c2609da006916815b4d743c6c
Felix Lisczyk <felix.lisczyk@gmail.com>
2023-11-17 15:50:57 +0100

    Added support for Xcode 15.1 beta

--- Package.swift
+++ Package.swift
@@ -3,18 +3,27 @@
 import PackageDescription
 
 func buildTargets() -> [Target] {
-    [
-        .binaryTarget(
-            name: "Realm",
-            url: "https://www.snipnotes.de/files/realm-swift/10.44.0/Realm.xcframework.zip",
-            checksum: "e0aa253174bb6d5f61333b36256654eaac16a358738ced143681e5778a387317"
-        ),
-        .binaryTarget(
+    let realmTarget: Target = .binaryTarget(
+        name: "Realm",
+        url: "https://github.com/realm/realm-swift/releases/download/v10.44.0/Realm.spm.zip",
+        checksum: "b26c979ce122acaabb6a6eef9567de43344377afa825c597ae6e14caf508c225"
+    )
+
+    #if swift(>=5.9.1)
+        let realmSwiftTarget: Target = .binaryTarget(
             name: "RealmSwift",
-            url: "https://www.snipnotes.de/files/realm-swift/10.44.0/RealmSwift.xcframework.zip",
-            checksum: "9728f22232398a18f33a6dd48ab99d1b02815ca55476fb9e34024e24fb017653"
+            url: "https://github.com/realm/realm-swift/releases/download/v10.44.0/RealmSwift@15.1.spm.zip",
+            checksum: "acc10a6a5e0dd285be81b487dc0ff8379f8abaf43bde769f9452843ddb81a383"
         )
-    ]
+    #else
+        let realmSwiftTarget: Target = .binaryTarget(
+            name: "RealmSwift",
+            url: "https://github.com/realm/realm-swift/releases/download/v10.44.0/RealmSwift@15.0.spm.zip",
+            checksum: "c548dd125df800b5888129f93119360cd3bb3b74ebd740bc21a170f411debee1"
+        )
+    #endif
+
+    return [realmTarget, realmSwiftTarget]
 }
 
 let package = Package(
