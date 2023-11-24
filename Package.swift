5b84b5a863c86ed0befc9f27edf914141f56556c
Felix Lisczyk <felix.lisczyk@gmail.com>
2023-11-24 19:43:08 +0100

    Restored self-hosted framework binaries
    `Realm.framework` 10.44.0 from GitHub is built with a beta release of Xcode which causes ASC to invalidate the binary

--- Package.swift
+++ Package.swift
@@ -3,23 +3,29 @@
 import PackageDescription
 
 func buildTargets() -> [Target] {
-    let realmTarget: Target = .binaryTarget(
-        name: "Realm",
-        url: "https://github.com/realm/realm-swift/releases/download/v10.44.0/Realm.spm.zip",
-        checksum: "b26c979ce122acaabb6a6eef9567de43344377afa825c597ae6e14caf508c225"
-    )
-
     #if swift(>=5.9.1)
+        // Xcode 15.1
+        let realmTarget: Target = .binaryTarget(
+            name: "Realm",
+            url: "https://www.snipnotes.de/files/realm/10.44.0/15.1/Realm.xcframework.zip",
+            checksum: "050f338bf16637858d661900170a5e9de0d308885103d7cefeee902a9128fb08"
+        )
         let realmSwiftTarget: Target = .binaryTarget(
             name: "RealmSwift",
-            url: "https://github.com/realm/realm-swift/releases/download/v10.44.0/RealmSwift@15.1.spm.zip",
-            checksum: "acc10a6a5e0dd285be81b487dc0ff8379f8abaf43bde769f9452843ddb81a383"
+            url: "https://www.snipnotes.de/files/realm/10.44.0/15.1/RealmSwift.xcframework.zip",
+            checksum: "940659d407d4933277c1efb8286710eceedf8f4de1e614223e6e1a3fdb6d0a40"
         )
     #else
+        // Xcode 15.0
+        let realmTarget: Target = .binaryTarget(
+            name: "Realm",
+            url: "https://www.snipnotes.de/files/realm/10.44.0/15.0/Realm.xcframework.zip",
+            checksum: "1e795e41a607de77f6032cc5dc93cf867ec43a1cc63b8458b2f4be8c9304fc4a"
+        )
         let realmSwiftTarget: Target = .binaryTarget(
             name: "RealmSwift",
-            url: "https://github.com/realm/realm-swift/releases/download/v10.44.0/RealmSwift@15.0.spm.zip",
-            checksum: "c548dd125df800b5888129f93119360cd3bb3b74ebd740bc21a170f411debee1"
+            url: "https://www.snipnotes.de/files/realm/10.44.0/15.0/RealmSwift.xcframework.zip",
+            checksum: "5a08edb76ac336d976ca8ac56e673d475a8db4e0bc9ca98ce252278b9db908e0"
         )
     #endif
 
