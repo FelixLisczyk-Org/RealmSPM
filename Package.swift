4c5052000e4783cb97410ed40cc5619fe99bf420
Felix Lisczyk <5102728+FelixLisczyk@users.noreply.github.com>
2024-04-04 11:17:37 +0200

    Updated Realm
    TSK-2096

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ import PackageDescription
 func buildTargets() -> [Target] {
     let realmTarget: Target = .binaryTarget(
         name: "Realm",
-        url: "https://github.com/realm/realm-swift/releases/download/v10.48.1/Realm.spm.zip",
-        checksum: "9df06aa37e11a366d58b819d12e02ab25e456944c6eb67609538f3175fe57d8c"
+        url: "https://github.com/realm/realm-swift/releases/download/v10.49.1/Realm.spm.zip",
+        checksum: "2b7854a2f9372a7d89495cf854c18a9d60b15ed1057afafecd24b6f4281634df"
     )
     let realmSwiftTarget: Target = .binaryTarget(
         name: "RealmSwift",
-        url: "https://github.com/realm/realm-swift/releases/download/v10.48.1/RealmSwift@15.3.spm.zip",
-        checksum: "f2b1aa3b94b1f999213f8dc6966cd1ce45dbe53568ec9db177a0df346e01c943"
+        url: "https://github.com/realm/realm-swift/releases/download/v10.49.1/RealmSwift@15.3.spm.zip",
+        checksum: "cc0c4b32cdec945593b53bd1c80770ec2b4dbe4385efbb864e9dd0691deb4fc5"
     )
 
     return [realmTarget, realmSwiftTarget]
