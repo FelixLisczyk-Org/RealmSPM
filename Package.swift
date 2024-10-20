c9d8ed408128f45db7b61909dfc4dfd93592cd4a
Felix Lisczyk <5102728+FelixLisczyk@users.noreply.github.com>
2024-10-20 13:37:27 +0200

    Added self-hosted version of Realm framework
    TSK-2557

--- Package.swift
+++ Package.swift
@@ -5,13 +5,13 @@ import PackageDescription
 func buildTargets() -> [Target] {
     let realmTarget: Target = .binaryTarget(
         name: "Realm",
-        url: "https://github.com/realm/realm-swift/releases/download/v20.0.0/Realm.spm.zip",
-        checksum: "11428c01a60a81fb6a314d562de86cf724cd114330c36bcb4415ed276332f176"
+        url: "https://www.snipnotes.de/files/realm/v20.0.0/Realm.xcframework.zip",
+        checksum: "8b7ffc1f7d7be7d6ad3506de278b869bbf6b6752b7ff7f15fe0da437287765db"
     )
     let realmSwiftTarget: Target = .binaryTarget(
         name: "RealmSwift",
-        url: "https://github.com/realm/realm-swift/releases/download/v20.0.0/RealmSwift@16_beta_6.spm.zip",
-        checksum: "a6e83219ddefd9731aaf39c6a59589cfa998665abdd241398c59c7ae83332253"
+        url: "https://www.snipnotes.de/files/realm/v20.0.0/RealmSwift.xcframework.zip",
+        checksum: "3b1a25253d95102b619184b2d1302dfc514c883f98e94295e36d605c528fc299"
     )
 
     return [realmTarget, realmSwiftTarget]
