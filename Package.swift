391bdd19eb3b27f90b91c643ac94e6e133d85674
Felix Lisczyk <5102728+FelixLisczyk@users.noreply.github.com>
2025-06-04 14:11:43 +0200

    Changed binary urls to fix checksum error [skip ci]

--- Package.swift
+++ Package.swift
@@ -5,12 +5,12 @@ import PackageDescription
 func buildTargets() -> [Target] {
     let realmTarget: Target = .binaryTarget(
         name: "Realm",
-        url: "https://www.snipnotes.de/files/realm/v20.0.2/xcode16-4/Realm.xcframework.zip",
+        url: "https://www.snipnotes.de/files/realm/v20.0.2/xcode16-4-0/Realm.xcframework.zip",
         checksum: "7f8301015d016690e3b5af80f2d90193a04fadbc6b3e03ade35900a55f52a076"
     )
     let realmSwiftTarget: Target = .binaryTarget(
         name: "RealmSwift",
-        url: "https://www.snipnotes.de/files/realm/v20.0.2/xcode16-4/RealmSwift.xcframework.zip",
+        url: "https://www.snipnotes.de/files/realm/v20.0.2/xcode16-4-0/RealmSwift.xcframework.zip",
         checksum: "bed085bd2c19db65727618bc960fddab493dc6acbcee93f7f208b91a34ddaea5"
     )
     return [realmTarget, realmSwiftTarget]
