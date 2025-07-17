83d11d85392fcb97f15ff86f5b031b723a6b3a8e
Felix Lisczyk <5102728+FelixLisczyk@users.noreply.github.com>
2025-07-17 09:36:07 +0200

    Added support for Xcode 26 / Swift 6.2
    Ref PL-125

--- Package.swift
+++ Package.swift
@@ -1,18 +1,31 @@
-// swift-tools-version:5.8
+// swift-tools-version:6.0
 
 import PackageDescription
 
 func buildTargets() -> [Target] {
-    let realmTarget: Target = .binaryTarget(
-        name: "Realm",
-        url: "https://www.snipnotes.de/files/realm/v20.0.2/xcode16-4-0/Realm.xcframework.zip",
-        checksum: "7f8301015d016690e3b5af80f2d90193a04fadbc6b3e03ade35900a55f52a076"
-    )
-    let realmSwiftTarget: Target = .binaryTarget(
-        name: "RealmSwift",
-        url: "https://www.snipnotes.de/files/realm/v20.0.2/xcode16-4-0/RealmSwift.xcframework.zip",
-        checksum: "bed085bd2c19db65727618bc960fddab493dc6acbcee93f7f208b91a34ddaea5"
-    )
+    #if swift(>=6.2)
+        let realmTarget: Target = .binaryTarget(
+            name: "Realm",
+            url: "https://www.snipnotes.de/files/realm/v20.0.3/xcode26-0/Realm.xcframework.zip",
+            checksum: "80bcdd7ba1b6c6c356962cdaf68a0f397880146014f760c1ff7f05862e90b7ec"
+        )
+        let realmSwiftTarget: Target = .binaryTarget(
+            name: "RealmSwift",
+            url: "https://www.snipnotes.de/files/realm/v20.0.3/xcode26-0/RealmSwift.xcframework.zip",
+            checksum: "5f3a5aa8c2546e2b1774c2a615d03fb608a76800cffde6432d9c1cd7d5620f84"
+        )
+    #else
+        let realmTarget: Target = .binaryTarget(
+            name: "Realm",
+            url: "https://www.snipnotes.de/files/realm/v20.0.2/xcode16-4-0/Realm.xcframework.zip",
+            checksum: "7f8301015d016690e3b5af80f2d90193a04fadbc6b3e03ade35900a55f52a076"
+        )
+        let realmSwiftTarget: Target = .binaryTarget(
+            name: "RealmSwift",
+            url: "https://www.snipnotes.de/files/realm/v20.0.2/xcode16-4-0/RealmSwift.xcframework.zip",
+            checksum: "bed085bd2c19db65727618bc960fddab493dc6acbcee93f7f208b91a34ddaea5"
+        )
+    #endif
     return [realmTarget, realmSwiftTarget]
 }
 
